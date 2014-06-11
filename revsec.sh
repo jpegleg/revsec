a2177caf316ef36287670fb3fbd12c43f40db6b5
Keegan Bowen <keegan.bowen@gmail.com>
2014-06-11 00:19:35 -0600

    Update revsec.sh

--- revsec.sh
+++ revsec.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Crawl internal file system for changes.
+# Crawl internal file system for changes by creating a catalog of diffs.
 # Usage:
-# make the scripts executable: chmod +x rev*sh
-# set the $target after the exec script like this to catalog /tmp
+# Make the scripts executable: chmod +x rev*sh
+# Set the $target after the exec script like this to catalog /tmp
 # ./revexec.sh /tmp
 # Then visit /var/low/crawl for your data.
 
