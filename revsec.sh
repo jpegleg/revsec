86c090a21719f626b1fd8b04afb4e75642f0d973
Keegan Bowen <keegan.bowen@gmail.com>
2014-06-11 05:22:47 -0600

    Update revsec.sh

--- revsec.sh
+++ revsec.sh
@@ -17,16 +17,16 @@ cd /
 chmod 777 /var/log/crawl/*
 
 function larthdiff {
-diffdir=$(pwd | cut -c2-999)
-echo /var/log/crawl/low"$diffdir" | xargs mkdir -p
-touch /var/log/crawl/low"$diffdir"/data.larth.prev
-touch /var/log/crawl/low"$diffdir"/data.larth
-ls -larth > /var/log/crawl/low"$diffdir"/data.larth
-diff /var/log/crawl/low"$diffdir"/data.larth /var/log/crawl/low"$diffdir"/data.larth.prev > /var/log/crawl/low"$diffdir"/data."$DATESTAMP".diff &&
-yes | cp /var/log/crawl/low"$diffdir"/data.larth /var/log/crawl/low"$diffdir"/data.larth.prev
-echo "$diffdir" >> /var/log/crawldiff.log;
-date >> /var/log/crawldiff.log;
-cat /var/log/crawl/low"$diffdir"/data."$DATESTAMP".diff >> /var/log/crawldiff.log
+    diffdir=$(pwd | cut -c2-999)
+    echo /var/log/crawl/low"$diffdir" | xargs mkdir -p
+    touch /var/log/crawl/low"$diffdir"/data.larth.prev
+    touch /var/log/crawl/low"$diffdir"/data.larth
+    ls -larth > /var/log/crawl/low"$diffdir"/data.larth
+    diff /var/log/crawl/low"$diffdir"/data.larth /var/log/crawl/low"$diffdir"/data.larth.prev > /var/log/crawl/low"$diffdir"/data."$DATESTAMP".diff &&
+    yes | cp /var/log/crawl/low"$diffdir"/data.larth /var/log/crawl/low"$diffdir"/data.larth.prev
+    echo "$diffdir" >> /var/log/crawldiff.log;
+    date >> /var/log/crawldiff.log;
+    cat /var/log/crawl/low"$diffdir"/data."$DATESTAMP".diff >> /var/log/crawldiff.log
 }
 
 larthdiff
@@ -37,8 +37,8 @@ larthdiff
 cp /dev/null /var/log/crawl/.pwd
 # Main loop.
 for target in "$@"; do
-find "$target"  >> /var/log/crawl/.pwd
-larthdiff
+    find "$target"  >> /var/log/crawl/.pwd
+    larthdiff
 done
 
 for y in $(cat /var/log/crawl/.pwd); do
