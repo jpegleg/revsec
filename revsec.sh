cbaa6a3fc0fa54e89abcf69fd300610f2532843f
Keegan Bowen <keegan.bowen@gmail.com>
2014-06-11 00:52:57 -0600

    naming and redirect corrections
    Change to static "data" file output for diff continuation...

--- revsec.sh
+++ revsec.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Crawl internal file system for changes by creating a catalog of diffs.
-# Usage:
-# Make the scripts executable: chmod +x rev*sh
-# Set the $target after the exec script like this to catalog /tmp
-# ./revexec.sh /tmp
-# Then visit /var/low/crawl for your data.
+# Crawl internal file system for changes.
+
+
 
 # Import the "strip" from my CryptoCore .bashrc but as stripdown
 
@@ -24,22 +21,21 @@ chmod 777 /var/log/crawl/*
 
 function larthdiff {
 diffdir=$(pwd | cut -c2-999)
-difffile="data""$DATESTAMP"
 echo /var/log/crawl/low"$diffdir" | xargs mkdir -p
-touch /var/log/crawl/low"$diffdir"/"$difffile".larth.prev
-touch /var/log/crawl/low"$diffdir"/"$difffile".larth
-cp /var/log/crawl/low"$diffdir"/"$difffile".larth /var/log/crawl/low"$diffdir"/"$difffile".larth.prev
-touch /var/log/crawl/low"$diffdir"/"$difffile".larth
-date >> /var/log/crawl/low"$diffdir"/"$difffile".larth
-ls -larth >> /var/log/crawl/low"$diffdir"/"$difffile".larth
-diff /var/log/crawl/low"$diffdir"/"$difffile".larth /var/log/crawl/low"$diffdir"/"$difffile".larth.prev > /var/log/crawl/low"$diffdir"/"$difffile"."$DATESTAMP"diff
-cat /var/log/crawl/low"$diffdir"/"$difffile"."$DATESTAMP"diff >> /var/log/crawldiff.log
+touch /var/log/crawl/low"$diffdir"/data.larth.prev
+touch /var/log/crawl/low"$diffdir"/data.larth
+cp /var/log/crawl/low"$diffdir"/data.larth /var/log/crawl/low"$diffdir"/data.larth.prev
+touch /var/log/crawl/low"$diffdir"/data.larth
+date >> /var/log/crawl/low"$diffdir"/data.larth
+ls -larth >> /var/log/crawl/low"$diffdir"/data.larth
+diff /var/log/crawl/low"$diffdir"/data.larth /var/log/crawl/low"$diffdir"/data.larth.prev > /var/log/crawl/low"$diffdir"/data."$DATESTAMP"diff
+cat /var/log/crawl/low"$diffdir"/data."$DATESTAMP"diff >> /var/log/crawldiff.log
 }
 
 function lsdirs {
-touch /var/log/crawl/low"$diffdir"/"$difffile".ls
-date >> /var/log/crawl/low"$diffdir"/"$difffile".ls
-ls > /var/log/crawl/low"$diffdir"/"$difffile".ls
+touch /var/log/crawl/low"$diffdir"/data.ls
+date > /var/log/crawl/low"$diffdir"/data.ls
+ls > /var/log/crawl/low"$diffdir"/data.ls
 }
 larthdiff
 lsdirs
@@ -61,5 +57,5 @@ done
 
 for y in $(cat /var/log/crawl/.pwd); do
     cd /"$y"
-    runit 
+    runit
 done
