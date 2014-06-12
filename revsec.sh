924f2cd1536831d22b82753f8cd0e095d16544d3
Keegan Bowen <keegan.bowen@gmail.com>
2014-06-12 12:15:31 -0600

    Update revsec.sh

--- revsec.sh
+++ revsec.sh
@@ -4,14 +4,11 @@
 
 mkdir -p /var/log/crawl/low 2> /dev/null
 touch /var/log/crawl/.filesystem
-chmod 755 /var/log/crawl
-chmod 755 /var/log/crawl/.filesystem
-chmod 755 /var/log/crawl/low
+
 diffdir=$(pwd | cut -f2-999)
 
 DATESTAMP=$(date '+%Y-%m-%d-%H-%M-%S')
 cd /
-chmod 755 /var/log/crawl/*
 
 function larthdiff {
     diffdir=$(pwd | cut -c2-999)
