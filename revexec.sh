30d90bb0bdcd538655d7f7b4a1c808d88c9e642f
Keegan Bowen <keegan.bowen@gmail.com>
2019-08-24 16:19:41 -0600

    Update revexec.sh

--- revexec.sh
+++ revexec.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 mkdir -p /var/log/crawl 2> /dev/null
 touch /var/log/crawl/.filesystem 2> /dev/null
 ./revsec.sh "$@" 2>&1  | cut -d':' -f4 | tee /var/log/crawl/.filesystem
